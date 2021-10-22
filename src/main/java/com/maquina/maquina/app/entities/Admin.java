819ea8c16902dc080eac1a007aa2d94323066dff
nolysprado <91513135+nolysprado@users.noreply.github.com>
2021-10-22 18:44:48 -0500

    Buenas practicas

--- src/main/java/com/maquina/maquina/app/entities/Admin.java
+++ src/main/java/com/maquina/maquina/app/entities/Admin.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Esta es la entidad de Admin
  */
 package com.maquina.maquina.app.entities;
 
--- src/main/java/com/maquina/maquina/app/services/AdminService.java
+++ src/main/java/com/maquina/maquina/app/services/AdminService.java
@@ -1,7 +1,5 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Servicio de Admin
  */
 package com.maquina.maquina.app.services;
 
