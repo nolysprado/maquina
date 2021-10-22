a244f5338e95f4c841e60bee6897f4d87e981bf6
nolysprado <91513135+nolysprado@users.noreply.github.com>
2021-10-21 21:12:52 -0500

    Cambios de cross

--- src/main/java/com/maquina/maquina/app/controllers/MessageController.java
+++ src/main/java/com/maquina/maquina/app/controllers/MessageController.java
@@ -14,6 +14,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -30,6 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("Message")
+@CrossOrigin(origins = "*")
 public class MessageController {
     @Autowired
     private MessageService service;
